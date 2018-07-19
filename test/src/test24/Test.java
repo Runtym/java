0bf7ca90849d4cf9e44cbf9513c1c75a82609f33
koitt03-A <koitt03-A@koitt03-A-PC>
2018-07-19 14:43:32 +0900

    adsf

--- test/src/test24/Test.java
+++ test/src/test24/Test.java
@@ -6,20 +6,26 @@ import java.util.HashMap;
 public class Test {
 
 	public static void main(String[] args) {
-		ArrayList<String> strList = new ArrayList<String>();
-		strList.add("abc1");
-		strList.add("abc2");
-		strList.add("abc3");
-		strList.add("abc4");
-		int idx = 1;
-		for(int i=0;i<strList.size();i++) {
-			String str = strList.get(i);
-			System.out.println("삭제 전 : " + strList);
-			if(str.equals("abc" + idx++)) {
-				strList.remove(i);
-				i--;
+		ArrayList<HashMap<String,String>> mList = 
+				new ArrayList<HashMap<String,String>>();
+		HashMap<String,String> hm = new HashMap<String,String>();
+		hm.put("name", "김길동");
+		mList.add(hm);
+		hm = new HashMap<String,String>();
+		hm.put("name", "이길동");
+		mList.add(hm);
+		hm = new HashMap<String,String>();
+		hm.put("name", "홍길동");
+		mList.add(hm);
+		
+		for(int i=0;i<mList.size();i++) {
+			HashMap<String,String> m = mList.get(i);
+			String name = m.get("name");
+			if(name.equals("홍길동")) {
+				System.out.println("홍길동님은 " + i + "번 위치에 있음!");
+				mList.remove(i);
 			}
-			System.out.println("삭제 후 : " + strList);
 		}
+		System.out.println(mList);
 	}
 }
