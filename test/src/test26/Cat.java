3977cb9d96cd4d79fa60ad9269b88438e5503247
koitt03-A <koitt03-A@koitt03-A-PC>
2018-07-23 17:53:30 +0900

    cat dog update(ToString())

--- test/src/test26/Cat.java
+++ test/src/test26/Cat.java
@@ -2,4 +2,7 @@ package test26;
 
 public class Cat extends Animal {
 
+	public String toString() {
+		return "난 고양이 : " + super.toString();
+	}
 }
--- test/src/test26/Dog.java
+++ test/src/test26/Dog.java
@@ -2,4 +2,7 @@ package test26;
 
 public class Dog extends Animal {
 
+	public String toString() {
+		return "난 개 : " + super.toString();
+	}
 }
